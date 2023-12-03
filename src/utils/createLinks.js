f9b4d7b58d547f890b458022552b6834aac2577f
ariannatnl <arianna.minucci@me.com>
2023-12-03 17:39:19 +0100

    tolto textdecoration dei link

--- src/utils/createLinks.js
+++ src/utils/createLinks.js
@@ -3,6 +3,7 @@ import { UIDesign } from "@zaionstate/ui/";
 export const createLinks = data => {
   const aDesing = new UIDesign({
     tag: "a",
+    className: "tx_d_n",
   });
   aDesing.setHtmlAttribute("target", data.target);
   aDesing.setHtmlAttribute("id", data.id);
