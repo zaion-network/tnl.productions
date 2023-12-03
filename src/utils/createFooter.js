e151daea40fed3dbe2c821c76c2e7d3fb0ca0a10
ariannatnl <arianna.minucci@me.com>
2023-12-03 16:40:16 +0100

    footer style

--- src/utils/createFooter.js
+++ src/utils/createFooter.js
@@ -2,7 +2,7 @@ import { UIDesign } from "@zaionstate/ui/";
 
 export const createFooter = () => {
   const FOOTERCLASSNAME =
-    "flex flex-column fontRoboto c-g fs_small p_1rem pc_e w_100% box_bor mt_10rem";
+    "flex flex-wrap fontRoboto c-g fs_small p_1rem w_100% box_bor jus_c_s_e mt_10rem";
   const footerDesign = new UIDesign({
     id: "footer",
     className: FOOTERCLASSNAME,
